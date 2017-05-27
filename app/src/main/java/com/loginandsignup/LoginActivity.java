1abeae1249d23050a0834978d224c0d6184653e0
Rozerin Köysu <rozerinkoysu@gmail.com>
2017-05-28 02:37:18 +0300

    custom progress dialog

--- app/src/main/java/com/loginandsignup/LoginActivity.java
+++ app/src/main/java/com/loginandsignup/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.loginandsignup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import ui.CustomProgressDialog;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, OnCompleteListener<AuthResult> {
 
@@ -52,24 +53,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
         btnLogin.setOnClickListener(this);
+        txtLink.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-        InputMethodManager imm = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
-//Hide:
-        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-//Show
-        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
-        validation();
-        if (isValid == true) {
-            btnLogin.setEnabled(true);
-            auth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
-                    .addOnCompleteListener(this);
-        } else
-            Toast.makeText(getApplicationContext(), "fill both of honey", Toast.LENGTH_LONG);
 
+        if (v == btnLogin) {
+            validation();
+            if (isValid == true) {
+                btnLogin.setEnabled(true);
+                auth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnCompleteListener(this);
+            } else {
+                Toast.makeText(getApplicationContext(), "fill both of honey", Toast.LENGTH_LONG);
+            }
+        } else {
+            Intent myIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(myIntent);
+
+        }
     }
 
     public void login() {
@@ -81,17 +85,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         email = String.valueOf(edtEmail.getText());
         password = String.valueOf(edtPassword.getText());
-        if (!email.equals("") || !password.equals("")) {
-            isValid=false;
+        if (email.equals("") || password.equals("")) {
+            isValid = false;
 
-        } else
+        } else {
             isValid = true;
+        }
         return isValid;
     }
 
     @Override
     public void onComplete(@NonNull Task<AuthResult> task) {
         if (task.isSuccessful()) {
+            CustomProgressDialog dialog = new CustomProgressDialog(getApplicationContext());
+            dialog.setIndeterminate(true);
+            dialog.setCancelable(false);
+            // return dialog;
+
             Toast.makeText(getApplicationContext(), "kayıt başarılı", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), " que paso? do it again ", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/ui/CustomProgressDialog.java
@@ -0,0 +1,36 @@
+package ui;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.loginandsignup.R;
+
+/**
+ * Created by Rozerin on 28.05.2017.
+ */
+
+public class CustomProgressDialog extends ProgressDialog {
+
+    private AnimationDrawable animationDrawable;
+
+    public CustomProgressDialog(Context context) {
+
+        super(context);
+    }
+
+    public CustomProgressDialog(Context context, int theme) {
+        super(context, theme);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_custom_progress_dialog);
+        ImageView la = (ImageView) findViewById(R.id.animation);
+        la.setBackgroundResource(R.drawable.progressBar);
+        animationDrawable = (AnimationDrawable) la.getBackground();
+    }
+}
