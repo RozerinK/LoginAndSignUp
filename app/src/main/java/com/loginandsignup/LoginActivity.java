1686846cbe088733ca1f32ac49dd9292fb46e450
Rozerin Köysu <rozerinkoysu@gmail.com>
2017-05-28 23:06:49 +0300

    color design / removing focus from edittext

--- app/src/main/java/com/loginandsignup/LoginActivity.java
+++ app/src/main/java/com/loginandsignup/LoginActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,13 +22,11 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import ui.CustomProgressDialog;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener, OnCompleteListener<AuthResult> {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, OnCompleteListener<AuthResult>, View.OnFocusChangeListener {
 
 // dont forget to remove loginactivity as a launcher on manifest.xml
 
-    String email;
-    ;
-    String password;
+
     @InjectView(R.id.activity_login_edtemail)
     EditText edtEmail;
     @InjectView(R.id.activity_login_edtpassword)
@@ -36,14 +37,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     TextView txtLink;
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     private boolean isValid;
-
+    private String _mail;
+    private String _password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
         getSupportActionBar().hide();
-
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         initEvent();
 
     }
@@ -54,6 +56,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         btnLogin.setOnClickListener(this);
         txtLink.setOnClickListener(this);
+        edtEmail.setOnFocusChangeListener(this);
+        edtPassword.setOnFocusChangeListener(this);
     }
 
 
@@ -61,7 +65,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
 
         if (v == btnLogin) {
-            validation();
+            validate();
             if (isValid == true) {
                 btnLogin.setEnabled(true);
                 auth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
@@ -81,14 +85,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    public boolean validation() {
+    public boolean validate() {
 
-        email = String.valueOf(edtEmail.getText());
-        password = String.valueOf(edtPassword.getText());
-        if (email.equals("") || password.equals("")) {
-            isValid = false;
-
+        _mail = String.valueOf(edtEmail.getText());
+        _password = String.valueOf(edtPassword.getText());
+        if (_mail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(_mail).matches()) {
+            edtEmail.setError("It is not an email address. No?");
         } else {
+            edtEmail.setError(null);
+            isValid = true;
+        }
+        if (_password.isEmpty() || _password.length() < 4 || _password.length() > 10) {
+            edtPassword.setError("do it strong men!");
+        } else {
+            edtPassword.setError(null);
             isValid = true;
         }
         return isValid;
@@ -97,14 +107,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onComplete(@NonNull Task<AuthResult> task) {
         if (task.isSuccessful()) {
-            CustomProgressDialog dialog = new CustomProgressDialog(getApplicationContext());
-            dialog.setIndeterminate(true);
-            dialog.setCancelable(false);
-            // return dialog;
+            callProgress();
 
-            Toast.makeText(getApplicationContext(), "kayıt başarılı", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), "kayıt başarılı", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), " que paso? do it again ", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public CustomProgressDialog callProgress() {
+        CustomProgressDialog dialog = new CustomProgressDialog(getApplicationContext());
+        dialog.setIndeterminate(true);
+        dialog.setCancelable(false);
+        return dialog;
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+
+        if (!hasFocus) {
+            hideKeyboard(v);
+        }
+    }
 }
+
